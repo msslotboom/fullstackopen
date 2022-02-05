5e9670dd589aa0ef31fb923f22d1b3b998d6c817
MikaelSlotboom <mikael.slotboom@helsinki.fi>
2022-02-05 13:56:20 +0200

    Exercise 2.11 fixed

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  const hook = () => {
+    axios
+    .get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook,[])
+
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -24,6 +30,7 @@ const App = () => {
     setPersons(persons.concat(personObject))
     }
   }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
