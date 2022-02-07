5a68eb449eed36053f39938f2b648212327a86c5
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-02-07 15:05:35 +0200

    Exercise 2.13

--- part2/maiden_tiedot/src/App.js
+++ part2/maiden_tiedot/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
 
   const [countries, setCountries] = useState([])
   const [filterValue, setFilter] = useState('')
-
+  const[oneCountry, setOneCountry] = useState([false,[]])
   useEffect(() => {
     countryService.getAll()
     .then(initialCountries => {
@@ -15,13 +15,14 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
+    setOneCountry(false,[])
   }
   
 
   return (
     <div>
       <Filter filterValue = {filterValue} handleFilterChange = {handleFilterChange}/>
-      <ListCountries filterValue = {filterValue} countries = {countries}/>
+      <ListCountries filterValue = {filterValue} countries = {countries} oneCountry = {oneCountry} setOneCountry = {setOneCountry}/>
     </div>
     
   );
@@ -34,23 +35,41 @@ const Filter = ({filterValue, handleFilterChange}) => {
   )
 }
 
-const ListCountries = ({countries, filterValue}) => {
+const ListCountries = ({countries, filterValue, oneCountry, setOneCountry}) => {
   const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(filterValue))
   const nrCountry = countriesToShow.length
-  if (nrCountry === 1){
-    const languageList = countriesToShow[0].languages
+  const showCountry = (country) => {
+    oneCountry = true
+    //console.log('countrytoshow',country)
+    const languageList = country.languages
     return(
       <div>
-        <h2>{countriesToShow[0].name.common}</h2>
-        capital {countriesToShow[0].capital}<br/>
-        area {countriesToShow[0].area}<br/>
+        <h2>{country.name.common}</h2>
+        capital {country.capital}<br/>
+        area {country.area}<br/>
         <h3>languages:</h3>
         {Object.keys(languageList).map(key =>
-          <li>{languageList[key]}</li>
+          <li key ={key}>{languageList[key]}</li>
         )}
         <br/>
-        <img src = {countriesToShow[0].flags.png} alt ='country flag' width="200" length="200"/>
+        <img src = {country.flags.png} alt ='country flag' width="200" length="200"/>
       </div>
+    )
+  }
+
+  //const showEvent = ({country}) => {
+  //  showCountry(country)
+  //}
+  //console.log(oneCountry)
+  if (oneCountry[0] === true){
+    return (
+      showCountry(oneCountry[1])
+    )
+  }
+  else if (nrCountry === 1){
+    //console.log('onecountry',countriesToShow[0])
+    return(
+    showCountry(countriesToShow[0])
     )
   }
   else if (nrCountry > 5){
@@ -60,7 +79,7 @@ const ListCountries = ({countries, filterValue}) => {
   }
   return(
     <>{countriesToShow.map(country =>
-      <div key = {country.name.common}>{country.name.common}</div>
+      <div key = {country.name.common}>{country.name.common}<button onClick={() => {setOneCountry([true, country])}}>Show</button></div>
     )}</>
   )
 }
