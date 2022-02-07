d7271346ab0cd0c83fe7ae80dca7cbae45263224
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-02-07 12:30:28 +0200

    Exercise 2.12

--- part2/maiden_tiedot/src/App.js
+++ part2/maiden_tiedot/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import countryService from './services/countries.js';
+import { useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [filterValue, setFilter] = useState('')
+
+  useEffect(() => {
+    countryService.getAll()
+    .then(initialCountries => {
+      setCountries(initialCountries)
+    })
+    },[])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Filter filterValue = {filterValue} handleFilterChange = {handleFilterChange}/>
+      <ListCountries filterValue = {filterValue} countries = {countries}/>
     </div>
+    
   );
+}
+const Filter = ({filterValue, handleFilterChange}) => {
+  return(
+    <div>
+    filter shown with <input value = {filterValue} onChange={handleFilterChange}/>
+    </div>
+  )
+}
+
+const ListCountries = ({countries, filterValue}) => {
+  const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(filterValue))
+  const nrCountry = countriesToShow.length
+  if (nrCountry === 1){
+    const languageList = countriesToShow[0].languages
+    return(
+      <div>
+        <h2>{countriesToShow[0].name.common}</h2>
+        capital {countriesToShow[0].capital}<br/>
+        area {countriesToShow[0].area}<br/>
+        <h3>languages:</h3>
+        {Object.keys(languageList).map(key =>
+          <li>{languageList[key]}</li>
+        )}
+        <br/>
+        <img src = {countriesToShow[0].flags.png} alt ='country flag' width="200" length="200"/>
+      </div>
+    )
+  }
+  else if (nrCountry > 5){
+    return(
+      <div>Too many matches, specify another filter</div>
+    )
+  }
+  return(
+    <>{countriesToShow.map(country =>
+      <div key = {country.name.common}>{country.name.common}</div>
+    )}</>
+  )
 }
 
 export default App;
