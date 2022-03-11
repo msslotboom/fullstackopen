34b7bc33a625c244ec91557d4196acde71faa111
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-03-11 16:06:02 +0200

    Backup of progress for Exercise 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,49 +3,31 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const morgan = require('morgan')
+const schema = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const Blog = schema.blogSchema
+const blogsRouter = require('./controllers/blogs')
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
+//____________________________________
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-  
-mongoose.connect(mongoUrl)
-  .then(
-    console.log('connected to MongoDB')
-  )
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
   })
-
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.use('/api/blogs', blogsRouter)
+app.use(morgan('tiny'))
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
