53ffa9d64274e1360598fce8eb6bbf79d900348b
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-03-12 11:42:30 +0200

    Exercise 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,27 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+
+const totalLikes = (blogs) => {
+    if (Array.isArray(blogs) || blogs.likes !== undefined){
+        total = blogs.reduce((accumulator, blog) => {
+            return accumulator + blog.likes;
+        },0)
+        if (typeof(total) !== 'undefined'){
+            return total
+        }
+        else{
+            return 0
+        }
+    }
+    else{
+        return 0
+    }
+}
+
+module.exports = {
+dummy, totalLikes
+}
