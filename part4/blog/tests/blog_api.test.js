bda8928ca1567e99d76a74f2ae498c234adad116
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-03-15 14:06:26 +0200

    Exercise 4.9

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -10,21 +10,34 @@ const initialBlogs = [
     author: 'Mikael',
     url: '/testblog',
     likes: 1
+},
+{
+    title: 'Second test blog',
+    author: 'Mikael',
+    url: '/secondtest',
+    likes: 0
 }
 ]
 
 beforeEach(async () => {
     await Blog.deleteMany({})
     let blogObject = new Blog(initialBlogs[0])
-    // await blogObject.save()
-    // blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
     await blogObject.save()})
 
 
 test('notes are returned as json of length 1', async () => {
   const response = await api.get('/api/blogs').expect('Content-Type', /application\/json/).expect(200)
   expect(response.body).toHaveLength(initialBlogs.length)
-    
+})
+
+test('id field has name id', async () => {
+    const response = await api.get('/api/blogs')
+    for (const blogpost of response.body){
+        // console.log(blogpost)
+        expect(blogpost.id).toBeDefined()
+    }
 })
 
 afterAll(() => {
