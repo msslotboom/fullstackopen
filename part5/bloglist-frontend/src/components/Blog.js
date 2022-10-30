f82e19f71ce7f4e56d89f6fba76b68e22dc6c320
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-10-30 09:42:19 +0200

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div key = {blog.id}>
-    {blog.title} {blog.author}
+import {useState} from 'react'
+
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  return(
+  <div key = {blog.id} style = {blogStyle}>
+    <div style = {hideWhenVisible}>
+      {blog.title} <button onClick={toggleVisibility}>View</button>
+    </div>
+
+    <div style = {showWhenVisible}>
+      {blog.title}  <button onClick={toggleVisibility}>Hide</button><br/>
+      {blog.url}<br/>
+      likes {blog.likes} <button>like</button><br/>
+      {blog.author}
+    </div>
+    
   </div>  
-)
+  )
+}
 
 export default Blog
