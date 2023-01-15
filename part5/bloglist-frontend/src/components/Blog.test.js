35f6f9b45731d1fcab171c553d5f90beee7231ab
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2023-01-15 22:23:25 +0200

    Exercise 5.13 another fix

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import { React } from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
@@ -9,14 +10,17 @@ test('renders title', () => {
     title: 'test blog',
     id: 1,
     author: 'dev',
-    url: '',
+    url: 'testurl',
     likes: 1
   }
 
   render(<Blog blog={blogObject} />)
-
   const element = screen.getByText('test blog')
-
   expect(element).toBeDefined()
+  const element2 = screen.queryByText('testurl')
+  expect(element2).toBeNull()
+  const element3 = screen.queryByText('dev')
+  expect(element3).toBeNull()
 
 })
+
