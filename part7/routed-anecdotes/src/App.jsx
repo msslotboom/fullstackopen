b4511224f5d9f0041ff8933cbeb5bdf8736c5a21
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2025-02-06 21:10:43 +0200

    Exercise 7.6

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -69,9 +69,9 @@ const Footer = () => (
 const CreateNew = (props) => {
   const navigate = useNavigate()
 
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const {reset: resetContent, ...content} = useField('text')
+  const {reset: resetAuthor, ...author} = useField('text')
+  const {reset: resetInfo, ...info} = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -102,6 +102,7 @@ const CreateNew = (props) => {
         </div>
         <button>create</button>
       </form>
+      <button onClick={() => {resetContent(); resetAuthor(); resetInfo();}}>reset</button>
     </div>
   )
 
